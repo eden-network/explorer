42731c59632d7f10bddbd77c6a9550dfbd99d4a2
John Carter <johncarter7061@gmail.com>
2021-09-02 21:12:58 -0600

    fix purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ const forms = require('@tailwindcss/forms');
 
 module.exports = {
   mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     colors: {
