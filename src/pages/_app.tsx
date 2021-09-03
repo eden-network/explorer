b979fd5799bdbaa5ad1ac869bdaf1e43419aeec3
John Carter <johncarter7061@gmail.com>
2021-09-03 09:53:17 -0600

    add progress bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,20 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import 'tailwindcss/tailwind.css';
+import NextNprogress from 'nextjs-progressbar';
+
 import Header from '../layout/Header';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Header />
+      <NextNprogress
+        color="#29D"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+        showOnShallow
+      />
       <Component {...pageProps} />
     </>
   );
