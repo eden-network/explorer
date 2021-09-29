7b5b58ff95e483b39e346294aae907b6033f09d7
Miha <mihalo3c@gmail.com>
2021-09-29 10:41:01 +0200

    Fix home blocks table

---
+++ src/components/BlocksHome.tsx
@@ -0,0 +1,93 @@
+import { useMemo } from 'react';
+
+import TimeAgo from 'javascript-time-ago';
+import en from 'javascript-time-ago/locale/en';
+import Link from 'next/link';
+
+import { getMinerAlias } from '../modules/getters';
+
+TimeAgo.addDefaultLocale(en);
+
+export default function Blocks({
+  blocks,
+}: {
+  blocks: {
+    number: number;
+    author: string;
+    timestamp: number;
+    slotTxs: number;
+    bundledTxs: number;
+    stakerTxs: number;
+  }[];
+}) {
+  const timeAgo = useMemo(() => new TimeAgo('en-US'), []);
+
+  const shapedBlocks = useMemo(
+    () =>
+      blocks.map((block) => {
+        return {
+          number: block.number,
+          author: getMinerAlias(block.author) || block.author,
+          timestamp: timeAgo.format(block.timestamp * 1000),
+          bundledTxs: block.bundledTxs,
+          stakerTxs: block.stakerTxs,
+          slotTxs: block.slotTxs,
+        };
+      }),
+    [blocks, timeAgo]
+  );
+
+  return (
+    <div className="flex flex-col">
+      <div className="-my-2 overflow-hidden sm:-mx-6 lg:-mx-8">
+        <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+          <div className="overflow-hidden sm:rounded-lg">
+            <table className="min-w-full">
+              <thead className="bg-blue-light">
+                <tr>
+                  <th
+                    scope="col"
+                    className="px-2 sm:px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Number
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-2 sm:px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Time
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-2 sm:px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Producer
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="divide-y divide-blue-light">
+                {shapedBlocks.map((block) => (
+                  <tr key={block.number}>
+                    <td className="px-2 sm:px-6 py-4 whitespace-nowrap">
+                      <Link href={`/block/${block.number}`}>
+                        <a className="text-green">
+                          {block.number.toLocaleString()}
+                        </a>
+                      </Link>
+                    </td>
+                    <td className="px-2 sm:px-6 py-4 whitespace-nowrap">
+                      {block.timestamp}
+                    </td>
+                    <td className="px-2 sm:px-6 py-4 whitespace-nowrap">
+                      {block.author}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
