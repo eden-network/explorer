1373d9274f129e83b7d79b5215578e4a8f0e171c
Miha <mihalo3c@gmail.com>
2021-10-04 11:35:40 +0200

    fix staker stat width

--- src/components/StakerHeroStats.tsx
+++ src/components/StakerHeroStats.tsx
@@ -12,9 +12,9 @@ export default function StakerHeroStats({
   return (
     <div className="mt-10 pb-12 sm:pb-16">
       <div className="relative">
-        <div className="relative max-w-20xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="relative max-w-30xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="max-w-4xl mx-auto">
-            <dl className="rounded-lg bg-green sm:grid sm:grid-cols-2 text-blue shadow-lg">
+            <dl className="rounded-lg bg-green sm:grid sm:grid-cols-4 text-blue shadow-lg">
               <div className="flex flex-col border-b border-blue p-6 text-center sm:border-0 sm:border-r">
                 <dt className="order-2 mt-2 text-lg leading-6 font-medium text-blue-light">
                   Rank
