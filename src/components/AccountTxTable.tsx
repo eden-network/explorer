785549f627c28668a3d5cbffa33d04fd855ddefa
John Carter <johncarter7061@gmail.com>
2021-10-06 07:37:20 -0400

    fix responsive issues

--- src/components/AccountTxTable.tsx
+++ src/components/AccountTxTable.tsx
@@ -13,9 +13,9 @@ const formatAddress = (address) => {
 export default function AccountTxTable({ transactions }) {
   return (
     <div className="flex flex-col">
-      <div className="-my-2 overflow-x-scroll md:overflow-hidden sm:-mx-6 lg:-mx-8">
+      <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
         <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-          <div className="overflow-x-scroll md:overflow-hidden sm:rounded-lg">
+          <div className="overflow-x-auto sm:rounded-lg">
             <table className="min-w-full">
               <thead className="bg-blue-light">
                 <tr>
