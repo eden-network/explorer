1c886fc2ba51b6ba41d1ffdbe5cb258917954f94
John Carter <johncarter7061@gmail.com>
2021-10-08 09:31:06 -0400

    clean code

--- src/hooks/usePrevious.hook.ts
+++ src/hooks/usePrevious.hook.ts
@@ -3,7 +3,6 @@ import { useEffect, useRef } from 'react';
 function usePrevious<T>(value: T): T {
   const ref = useRef<T>();
   useEffect(() => {
-    console.log('value changes', { value });
     ref.current = value;
   }, [value]);
   return ref.current as T;
