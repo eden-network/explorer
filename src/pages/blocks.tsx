8e047ed6877a5f2165aab7c85fe55b1886c411ae
John Carter <johncarter7061@gmail.com>
2021-11-22 14:24:01 -0500

    remove local-storage filter saving

--- src/pages/blocks.tsx
+++ src/pages/blocks.tsx
@@ -10,7 +10,6 @@ import AutoCompleteInput from '../components/AutocompleteInput';
 import Blocks from '../components/Blocks';
 import Chip from '../components/Chip';
 import EndlessPagination from '../components/EndlessPagination';
-import useLocalStorage from '../hooks/useLocalStorage.hook';
 import { Meta } from '../layout/Meta';
 import Shell from '../layout/Shell';
 import { getBlockInsightAndCache } from '../modules/eden-block-insight';
@@ -24,26 +23,17 @@ const PER_PAGE = 10;
 
 const MINERS = Object.values(minerAlias);
 
-export const LocalStorageKeyMinerFilter = 'block-list-miner-filters';
-const LocalStorageKeyMinerFilterProducerFilter = 'block-list-producer-filter';
-
 export default function BlocksPage({ blocks }) {
   const router = useRouter();
   const [beforeEpoch, setbeforeEpoch] = useState(() => {
     return router.query.beforeEpoch ?? new Date().getTime() / 1e3;
   });
 
-  const [miners, setMiners] = useLocalStorage<string[]>(
-    LocalStorageKeyMinerFilter,
-    () => {
-      if (router.query.miner) return [router.query.miner as string];
-      return [];
-    }
-  );
-  const [edenProducerOnly, setEdenProducerOnly] = useLocalStorage(
-    LocalStorageKeyMinerFilterProducerFilter,
-    true
-  );
+  const [miners, setMiners] = useState<string[]>(() => {
+    if (router.query.miner) return [router.query.miner as string];
+    return [];
+  });
+  const [edenProducerOnly, setEdenProducerOnly] = useState(true);
   const [inputValue, setInputValue] = useState('');
   const [selectedVal, setSelectedVal] = useState('');
 
